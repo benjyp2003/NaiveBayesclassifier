50b6a26182263fa6d3ceb86108ca116b306efac8
benjyp2003 <benjypfeffer@gmail.com>
2025-07-26 23:51:34 +0300

    deleted unwanted folders

---
+++ building_app/ui/cli.py
@@ -0,0 +1,18 @@
+
+class CLI:
+
+    @staticmethod
+    def show_menu():
+        print()
+        print("=" * 40)
+        print("|{:^38}|".format("MAIN MENU"))
+        print("=" * 40)
+        print("| 1 - Train new data set               |")
+        print("| 2 - Assert new data to classify      |")
+        print("| 3 - Exit                             |")
+        print("=" * 40)
+
+
+
+
+
